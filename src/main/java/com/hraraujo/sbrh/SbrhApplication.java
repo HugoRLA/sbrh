2a2abc3e27fc6eac9476f7024bf7efa6de9b4362
Hugo Araujo <hugoricardola@gmail.com>
2019-10-15 23:25:19 +0100

    Initial commit

---
+++ src/main/java/com/hraraujo/sbrh/SbrhApplication.java
@@ -0,0 +1,13 @@
+package com.hraraujo.sbrh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbrhApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SbrhApplication.class, args);
+    }
+
+}
